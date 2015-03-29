e055b43e6826d589026030f18f720aa3531be122
Kamal Marhubi <kamal@marhubi.com>
2015-03-29 18:40:55 -0400

    Use Doxygen-style documentation comments in utils.h

--- utils.h
+++ utils.h
@@ -4,28 +4,39 @@
 #define MAX_LEN 1024
 #define TOKEN_SEP " \t\n\r"
 
-// Struct to represent a command and its arguments.
+/**
+ * Struct to represent a command and its arguments.
+ */
 typedef struct {
+  /** The name of the executable. */
   char* progname;
+  /** The arguments; must be NULL-terminated. */
   char* args[];
 } cmd_struct;
 
-// Struct to represent a pipeline of commands. It should be understood that
-// cmd[i]'s output goes to cmd[i+1]'s input. The total number of commands is
-// stored in n_cmds.
+/**
+ * Struct to represent a pipeline of commands. The intention is that cmd[i]'s
+ * output goes to cmd[i+1]'s input.
+ */
 typedef struct {
+  /** The total number of commands. */
   int n_cmds;
+  /** The commands themselves. */
   cmd_struct* cmds[];
 } pipeline_struct;
 
-// Parses str into a freshly allocated cmd_struct and returns a pointer to it.
+/**
+ * Parses str into a freshly allocated cmd_struct and returns a pointer to it.
+ */
 cmd_struct* parse_command(char* str);
 
-// Parses str into a freshly allocated pipeline_struct and returns a pointer to it.
-// All cmd_structs in cmds will also be freshy allocated.
+/**
+ * Parses str into a freshly allocated pipeline_struct and returns a pointer to
+ * it.  All cmd_structs in cmds will also be freshy allocated.
+ */
 pipeline_struct* parse_pipeline(char* str);
 
-// For debugging purposes.
+/* For debugging purposes. */
 void print_command(cmd_struct* command);
 void print_pipeline(pipeline_struct* pipeline);
 
