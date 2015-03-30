9dfc02c60e7e385bd044b1ba054ab5494070a405
Kamal Marhubi <kamal@marhubi.com>
2015-03-30 08:07:16 -0400

    Add support for command pipelines

--- shell.c
+++ shell.c
@@ -6,10 +6,6 @@
 
 #include "utils.h"
 
-int my_exec(char* progname, char** args) {
-  return execvp(progname, args);
-}
-
 
 ssize_t prompt_and_get_input(const char* prompt,
                             char **line,
@@ -19,20 +15,28 @@ ssize_t prompt_and_get_input(const char* prompt,
 }
 
 
-int exec_with_redir(cmd_struct* command, int in_fd, int out_fd) {
-  if (in_fd != -1) {
-    dup2(in_fd, STDIN_FILENO);
-    close(in_fd);
+void close_ALL_the_pipes(int n_pipes, int (*pipes)[2]) {
+  for (int i = 0; i < n_pipes; ++i) {
+    close(pipes[i][0]);
+    close(pipes[i][1]);
   }
-  if (out_fd != -1) {
-    dup2(out_fd, STDOUT_FILENO);
-    close(out_fd);
+}
+
+
+int exec_with_redir(cmd_struct* command, int n_pipes, int (*pipes)[2]) {
+  int fd = -1;
+  if ((fd = command->redirect[0]) != -1) {
+    dup2(fd, STDIN_FILENO);
   }
+  if ((fd = command->redirect[1]) != -1) {
+    dup2(fd, STDOUT_FILENO);
+  }
+  close_ALL_the_pipes(n_pipes, pipes);
   return execvp(command->progname, command->args);
 }
 
 
-pid_t run_with_redir(cmd_struct* command, int in_fd, int out_fd) {
+pid_t run_with_redir(cmd_struct* command, int n_pipes, int (*pipes)[2]) {
   pid_t child_pid = fork();
 
   if (child_pid) {  /* We are the parent. */
@@ -44,8 +48,7 @@ pid_t run_with_redir(cmd_struct* command, int in_fd, int out_fd) {
         return child_pid;
     }
   } else {  // We are the child. */
-    fputs("about to exec!\n", stderr);
-    exec_with_redir(command, in_fd, out_fd);
+    exec_with_redir(command, n_pipes, pipes);
     perror("OH DEAR");
     return 0;
   }
@@ -53,32 +56,33 @@ pid_t run_with_redir(cmd_struct* command, int in_fd, int out_fd) {
 
 
 int main() {
-  cmd_struct* command1, *command2;
   char *line = NULL;
   size_t len = 0;
 
   while(prompt_and_get_input("heeee> ", &line, &len) > 0) {
-    fprintf(stderr, "%s\n", line);
     pipeline_struct* pipeline = parse_pipeline(line);
-    print_pipeline(pipeline);
-    continue;
-    int ends[2] = { -1, -1 };
-    command1 = pipeline->cmds[0];
-    if (pipeline->n_cmds == 2) {
-      command2 = pipeline->cmds[1];
+    int n_pipes = pipeline->n_cmds - 1;
+    //print_pipeline(pipeline);
 
-      pipe(ends);
-      fprintf(stderr, "in: %d\nout: %d\n", ends[0], ends[1]);
+    /* pipes[i] redirects from pipeline->cmds[i] to pipeline->cmds[i+1]. */
+    int (*pipes)[2] = calloc(sizeof(int[2]), n_pipes);
+
+    for (int i = 1; i < pipeline->n_cmds; ++i) {
+      pipe(pipes[i-1]);  
+      pipeline->cmds[i]->redirect[STDIN_FILENO] = pipes[i-1][0];
+      pipeline->cmds[i-1]->redirect[STDOUT_FILENO] = pipes[i-1][1];
     }
 
-    run_with_redir(command1, -1, ends[1]);
-    if (pipeline->n_cmds == 2) {
-      run_with_redir(command2, ends[0], -1);
-      close(ends[0]);
-      close(ends[1]);
+    for (int i = 0; i < pipeline->n_cmds; ++i) {
+      run_with_redir(pipeline->cmds[i], n_pipes, pipes);
     }
-    //run_with_redir(command1, -1, -1);
-    while (wait(NULL) > 0);
+
+    close_ALL_the_pipes(n_pipes, pipes);
+
+    /* Wait for all the children to terminate. Rule 0: not checking status. */
+    for (int i = 0; i < pipeline->n_cmds; ++i) {
+      wait(NULL);
+    }
     
   }
   fputs("\n", stderr);
